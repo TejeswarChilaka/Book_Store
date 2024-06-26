625bad6f835ff3904e3353d4777823b24bb0658e
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-06-26 19:00:14 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to Book-Store");
+});
