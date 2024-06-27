1794a5253335c556e9b47578bb950c9cc30aaad7
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-06-27 08:40:50 +0530

    Updating the books in database using PUT  method.

--- server/index.js
+++ server/index.js
@@ -13,25 +13,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/books", async (req, res) => {
-    try {
-      const { title, author, publishYear } = req.body;
-      
-      if (!title || !author || !publishYear) {
-        return res.status(400).send({
-          message: "Send all required fields: title, author, publishYear",
-        });
-      }
-      
-      const newBook = { title, author, publishYear };
-      const book = await Book.create(newBook);
-      
-      return res.status(201).send(book); // Send the created book with 201 status
-    } catch (error) {
-      console.log(error.message);
-      return res.status(500).send({ message: error.message });
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
     }
-  });
-  
+
+    const newBook = { title, author, publishYear };
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book); // Send the created book with 201 status
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
 // app.post("/books", async (req, res) => {
 //   try {
 //     if (!req.body.title || !req.body.author || !req.body.publishYear) {
@@ -51,19 +51,48 @@ app.post("/books", async (req, res) => {
 //   }
 // });
 
-app.get(
-    '/books', async (req,res) => {
-        try{
-            const books = await Book.find({});
-            return res.status(200).json(books);
-        }
-        catch(error){
-            console.log(error.message);
-            res.status(500).send({message: error.message});
-        }
-    }
-);
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
 
+    return res.status(200).json({ count: books.length, data: books });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book updated succesfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
